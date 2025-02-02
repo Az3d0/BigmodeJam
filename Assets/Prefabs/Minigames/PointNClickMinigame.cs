e8f5a432be09e12b0a64802e5140bb49101ac0c3
Az3d0 <szmate158@gmail.com>
2025-02-02 13:57:24 +0000

    Small Restructure

--- Assets/Prefabs/Minigames/PointNClickMinigame.cs
+++ Assets/Prefabs/Minigames/PointNClickMinigame.cs
@@ -5,27 +5,44 @@ using UnityEngine.InputSystem;
 public class PointNClickMinigame : Minigame
 {
     protected DragableObject m_draggedObject;
-    protected event Action<GameObject> OnObjectHit;
 
+    [SerializeField] protected GameObject m_mouseFollowerAsset;
+    protected SpriteRenderer m_mouseFollowerSpriteRenderer;
+    [SerializeField] protected Sprite m_mouseFollowerSprite_Normal;
+    [SerializeField] protected Sprite m_mouseFollowerSprite_Clicked;
+    protected GameObject m_mouseFollower;
 
     protected virtual void Awake()
     {
 
         m_inputs = new InputSystem_Actions();
         m_inputs.Minigame1.Enable();
-        m_inputs.Minigame1.Select.performed += OnCLick;
+        m_inputs.Minigame1.Select.started += OnCLick;
         m_inputs.Minigame1.Select.canceled += OnRelease;
 
+        CreateMouseFollower();
     }
 
+    protected virtual void FixedUpdate()
+    {
+        if (m_mouseFollowerAsset == null) return;
+        Vector3 mousePosition = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, -5);
+        m_mouseFollower.transform.localPosition = mousePosition;
+    }
     protected virtual void OnCLick(InputAction.CallbackContext context)
     {
         RaycastFromMouse(context);
+
+        if (m_mouseFollowerAsset == null) return;
+        m_mouseFollowerSpriteRenderer.sprite = m_mouseFollowerSprite_Clicked;
     }
 
     protected virtual void OnRelease(InputAction.CallbackContext context)
     {
         ResetDragableObject(context);
+
+        if (m_mouseFollowerAsset == null) return;
+        m_mouseFollowerSpriteRenderer.sprite = m_mouseFollowerSprite_Normal;
     }
     protected override void OnDestroy()
     {
@@ -50,23 +67,38 @@ public class PointNClickMinigame : Minigame
 
         if (hit.collider != null)
         {
-            OnObjectHit?.Invoke(hit.collider.gameObject);
+            GameObject hitGO = hit.collider.gameObject;
+
+            if (hitGO.TryGetComponent(out ClickableObject clickableObject))
+            {
+                clickableObject.OnClicked();
+            }
+            else if (hitGO.TryGetComponent(out DragableObject dragableObject))
+            {
+                m_draggedObject = dragableObject;
+                m_draggedObject.SetIsBeingDragged(true);
+            }
         }
     }
 
-    protected virtual void SetClickObject(GameObject hitGO)
+    private void CreateMouseFollower()
     {
-        if (hitGO.TryGetComponent(out ClickableObject clickableObject))
+        if (m_mouseFollowerAsset == null) return;
+
+        m_mouseFollower = Instantiate(m_mouseFollowerAsset);
+        if (m_mouseFollower.TryGetComponent(out SpriteRenderer spriteRenderer))
         {
-            clickableObject.OnClicked();
+            m_mouseFollowerSpriteRenderer = spriteRenderer;
         }
-    }
-    protected virtual void SetDragableObject(GameObject hitGO)
-    {
-        if (hitGO.TryGetComponent(out DragableObject dragableObject))
+        else if (m_mouseFollower.transform.GetChild(0).TryGetComponent(out spriteRenderer))
         {
-            m_draggedObject = dragableObject;
-            m_draggedObject.SetIsBeingDragged(true);
+            m_mouseFollowerSpriteRenderer = spriteRenderer;
         }
+        else
+        {
+            Debug.Log("No spriteRenderer attached");
+        }
+        m_mouseFollower.transform.parent = transform;
+        m_mouseFollower.transform.position = gameObject.transform.position;
     }
 }
